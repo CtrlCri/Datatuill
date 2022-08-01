58c85de1d3f857e0c0e042d3305176ccb7e3304d
armycrih <armycrih@gmail.com>
2022-08-01 17:40:49 -0300

    Add models

--- dataset/models.py
+++ dataset/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Dataset(models.Model):
+    title = models.CharField(max_length=80)
+    slug_title = models.SlugField
+
+    code = models.TextField(blank=False)
+    about = models.TextField(blank=False)
+    
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    
+    num_downloads = models.IntegerField(default=0)
+
+    posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+
+class Like(models.Model):
+    user =models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    dataset = models.ForeignKey(Dataset, related_name='likes', on_delete=models.CASCADE)
+
